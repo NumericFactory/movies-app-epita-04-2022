afc81738772bf3325288cf43de100c487966920b
NumericFactory <frederic.lossignol@gmail.com>
2022-04-19 16:30:09 +0200

    first commit

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieModel } from '../models/movie.model';
 import { MovieService } from '../services/movie.service';
+
+
 
 @Component({
   selector: 'app-list',
@@ -7,12 +10,15 @@ import { MovieService } from '../services/movie.service';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
-
-  movies:Array<any> = []
+ 
+  movies:Array<MovieModel> = []
   /* injecter un objet http de la class HttpClient*/ 
   constructor(private movieSvc:MovieService) { }
 
   ngOnInit(): void {
+
+
+
     // 1 on fait la request HTTP à theMovieDB
     this.movieSvc.getMoviesFromApi();
     // 2 on s'abonne à movies$ (un Observable qui contient les movies)
---
+++ src/app/models/movie.model.ts
@@ -0,0 +1,33 @@
+/* 
+    Pour créer nos modeles de données côté front, on peut utiliser:
+    > les interface: permet le typage 
+    > les class : permet le typage ET instancier un objet
+*/
+
+// export interface MovieModel {
+//   id:number;
+//    titre:string;
+//    description: string;
+//    image: string;
+//    score: number;
+//    date: Date;
+// }
+
+export class MovieModel {
+    id:number;
+    titre:string;
+    description: string;
+    image: string;
+    score: number;
+    date: Date;
+
+    constructor(movieFromApi:any) {
+        this.id = movieFromApi.id;
+        this.titre = movieFromApi.title;
+        this.description = movieFromApi.overview;
+        this.image = movieFromApi.backdrop_path;
+        this.score = movieFromApi.vote_average;
+        this.date = movieFromApi.release_date;
+    }
+}
+
