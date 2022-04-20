f94eedbada554a279935ae8c0a4dbfe4a52b5195
NumericFactory <frederic.lossignol@gmail.com>
2022-04-20 13:51:12 +0200

    routes

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, SecurityContext } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 import { MovieService } from '../services/movie.service';
 
@@ -14,14 +15,25 @@ export class DetailComponent implements OnInit {
 
   constructor(
     private activatedRoute:ActivatedRoute, 
-    private movieSvc:MovieService
+    private movieSvc:MovieService,
     ) { }
 
   ngOnInit(): void {
-    console.log(this.activatedRoute.snapshot.params); 
-    // renvoie un objet des paramètres nommés de l'url {id:1234}
+    this.getVideoUrl('123')
+    console.log(this.activatedRoute.snapshot.params); // renvoie un objet des paramètres nommés de l'url {id:1234}
+    
+    // 1 Récupérer l'ID de la vidéo dans l'URL
     this.movieId = this.activatedRoute.snapshot.params['id'];
-    // Faire la request pour récuperer la listes des videos B.A 
+
+    // 2 Faire la request pour récuperer la listes des videos B.A 
+    /*
+      Ici, on consomme directement le retour de movieSvc.getVideosOfMovie()
+      à l'aide de la méthode .subscribe( response => doWhatYouWantWhenResponseWillArrive :) )
+      car nous avons juste besoin de faire une request HTTP à l'API TMDB
+
+      Si nous avions besoin d'écouter un changement ultérieur,
+      il serait préférable de mettre en place un Subject() côté service
+    */
     this.movieSvc.getVideosOfMovie(this.movieId).subscribe( 
       response => {
         this.movieVideo = response.results.find( (el:any) =>  el.site == 'YouTube') 
@@ -31,7 +43,7 @@ export class DetailComponent implements OnInit {
   }
 
   getVideoUrl(videoKey:string) {
-    return 'https://www.youtube.com/embed/'+videoKey
+    let url = 'https://www.youtube.com/embed/'+videoKey;
   }
 
 }
