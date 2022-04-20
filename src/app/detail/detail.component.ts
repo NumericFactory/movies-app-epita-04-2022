8dd39e3eabab5e4977bd8e09a25dcb546abadff1
NumericFactory <frederic.lossignol@gmail.com>
2022-04-20 11:52:44 +0200

    routes

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-detail',
@@ -10,14 +11,19 @@ export class DetailComponent implements OnInit {
 
   movieId:number = 0
 
-  constructor(private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private activatedRoute:ActivatedRoute, 
+    private movieSvc:MovieService
+    ) { }
 
   ngOnInit(): void {
     console.log(this.activatedRoute.snapshot.params); 
     // renvoie un objet des paramètres nommés de l'url {id:1234}
     this.movieId = this.activatedRoute.snapshot.params['id'];
     // Faire la request pour récuperer la listes des videos B.A 
-    // /movie/{id}/videos
+    this.movieSvc.getVideosOfMovie(this.movieId).subscribe( 
+      response => console.log(response)
+    )
 
   
   }
