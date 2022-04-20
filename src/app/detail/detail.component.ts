cae8df0e18b1d617d3f0cc07daf04e03754f321b
NumericFactory <frederic.lossignol@gmail.com>
2022-04-20 10:45:55 +0200

    routes

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detail',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(private activatedRoute:ActivatedRoute) { }
 
   ngOnInit(): void {
+    console.log(this.activatedRoute.snapshot.params)
+    
   }
 
 }
